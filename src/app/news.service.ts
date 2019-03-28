0b01e8eeb4d7a7a2883f8c9c39c73e9af659d682
Anet <Anet@A33COMP14.step.itstep.rv.ua>
2019-03-28 16:26:03 +0200

    add style

--- src/app/news.service.ts
+++ src/app/news.service.ts
@@ -1,9 +1,22 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Injectable({
   providedIn: 'root'
 })
 export class NewsService {
+  apiKey='ccd16b6b699f4a4592d8d4c06d57c93f';
+  apiUrl='https://newsapi.org/v2/';
+  constructor(private client:HttpClient) { }
 
-  constructor() { }
+  initSources(){
+    return this.client.get(this.apiUrl+ 'sources?language=en&apiKey=' +this.apiKey);
+  }
+  initArticles(){
+    return this.client.get(this.apiUrl + 'top-headlines?sources=techcrunch&apiKey=' + this.apiKey);
+  }
+  getArticlesById(source: string)
+  {
+    return this.client.get(this.apiUrl + 'top-headlines?sources=' + source + '&apiKey' +this.apiKey);
+  }
 }
